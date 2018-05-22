c602c58ebe4b4ea0f344fcd6664733b561e90b7e
KiviKnuuti <kivi.knuuti@gmail.com>
2018-05-22 16:12:54 +0300

    celsius

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -23,7 +23,7 @@ const getChats = async () => {
 
 const Chats = (props) => {
   console.log(props.chats)
-  const ChatList = props.chats.map(c => <li><b>Temperature:</b> {Math.round(parseFloat(c.message))} Time: {c.createdAt}</li>);
+  const ChatList = props.chats.map(c => <li><b>Temperature:</b> {Math.round(parseFloat(c.message))}°C Time: {c.createdAt}</li>);
   return <ul>{ChatList}</ul>; 
 };
 
